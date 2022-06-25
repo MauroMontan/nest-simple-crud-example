ca032870f45bf9197e410214e6c04fb66a25238b
MauroMontan <mauroanastacio@outlook.es>
2022-06-25 15:52:49 -0500

    acat entity created

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './entities/cat.entity';
 
 @Module({
   providers: [CatsService],
   controllers: [CatsController],
+  imports: [TypeOrmModule.forFeature([Cat])],
 })
 export class CatsModule {}
---
+++ src/cats/entities/cat.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cat {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  owner_email: string;
+
+  @Column()
+  name: string;
+}
