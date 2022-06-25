34c3a70ed6419108e5891f6f6404fd32e7a74348
MauroMontan <mauroanastacio@outlook.es>
2022-06-25 14:24:47 -0500

    catlist controller created

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private service: CatsService) {}
+  @Get()
+  catList() {
+    return this.service.getCatList();
+  }
+}
